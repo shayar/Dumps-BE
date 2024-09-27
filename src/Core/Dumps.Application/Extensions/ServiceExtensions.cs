51120a55a4ea15224ddd92578391ae699c709c39
Corion <corion.pradhan@gmail.com>
2024-09-27 14:28:43 -0400

    Merge conflict mistake solved

--- src/Core/Dumps.Application/Extensions/ServiceExtensions.cs
+++ src/Core/Dumps.Application/Extensions/ServiceExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System.Reflection;
+using Dumps.Application.Command;
 using Dumps.Domain.Entities;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,13 +15,11 @@ namespace Dumps.Application.Extensions
             // Configuration of MediatR
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
 
+            services.AddFluentValidationAutoValidation()
+              .AddValidatorsFromAssemblyContaining<
+                  CreateContactUsCommand>(); // registers validators from Assembly where CreateContactUsCommand is locatedinitia
             // Register any application-specific services here
             // services.AddScoped<IApplicationService, ApplicationService>();
-
-
-
-
-
             // Register PasswordHasher
             services.AddScoped<IPasswordHasher<ApplicationUser>, PasswordHasher<ApplicationUser>>();
             return services;
