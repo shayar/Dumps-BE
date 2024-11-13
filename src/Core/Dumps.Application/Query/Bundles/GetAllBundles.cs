24b32a1545fe6101d75dc8073620b6031d72722a
Shayar Shrestha <shayarshrestha23@gmail.com>
2024-11-12 23:21:58 -0500

    API to retrieve bundles

---
+++ src/Core/Dumps.Application/Query/Bundles/GetAllBundles.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Dumps.Application.APIResponse;
+using Dumps.Application.DTO.Response.Bundles;
+using Dumps.Application.Exceptions;
+using Dumps.Domain.Entities;
+using Dumps.Persistence.DbContext;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Dumps.Application.Query.Bundles
+{
+    public class GetAllBundles
+    {
+        public class GetAllBundlesQuery : IRequest<APIResponse<IList<CreateBundleResponse>>>
+        {
+        }
+
+        public class GetAllBundlesQueryHandler : IRequestHandler<GetAllBundlesQuery, APIResponse<IList<CreateBundleResponse>>>
+        {
+            private readonly AppDbContext _dbContext;
+            private readonly ILogger<GetAllBundlesQueryHandler> _logger;
+
+            public GetAllBundlesQueryHandler(AppDbContext dbContext, ILogger<GetAllBundlesQueryHandler> logger)
+            {
+                _dbContext = dbContext;
+                _logger = logger;
+            }
+
+            public async Task<APIResponse<IList<CreateBundleResponse>>> Handle(GetAllBundlesQuery request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    // Filter only non-deleted bundles
+                    Expression<Func<Bundle, bool>> filter = x => !x.IsDeleted;
+
+                    var bundlesList = await _dbContext.Bundles
+                        .Where(filter)
+                        .Include(b => b.BundlesProducts)
+                        .ThenInclude(bp => bp.Product)
+                        .OrderByDescending(b => b.CreatedAt)
+                        .Select(b => new CreateBundleResponse
+                        {
+                            Id = b.Id,
+                            Title = b.Title,
+                            Description = b.Description,
+                            DiscountedPrice = b.DiscountedPrice,
+                            ProductIds = b.BundlesProducts.Select(bp => bp.ProductId).ToList()
+                        })
+                        .ToListAsync(cancellationToken)
+                        .ConfigureAwait(false);
+
+                    return new APIResponse<IList<CreateBundleResponse>>(bundlesList, "Bundles retrieved successfully.");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error occurred while retrieving all bundles.");
+                    throw new RestException(HttpStatusCode.InternalServerError, "Cannot retrieve bundles.");
+                }
+            }
+        }
+    }
+}
--- src/Presentation/Dumps.API/Controllers/BundlesController.cs
+++ src/Presentation/Dumps.API/Controllers/BundlesController.cs
@@ -1,6 +1,9 @@
-﻿using Dumps.Application.Command.Bundles;
+﻿using Dumps.Application.APIResponse;
+using System.Net;
+using Dumps.Application.Command.Bundles;
 using Dumps.Application.DTO.Request.Bundles;
 using Dumps.Application.DTO.Response.Bundles;
+using Dumps.Application.Query.Bundles;
 using Dumps.Domain.Common.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,12 +12,47 @@ namespace Dumps.API.Controllers
 {
     public class BundlesController : BaseController
     {
+        /// <summary>
+        /// Create a new bundle (Admin only)
+        /// </summary>
+        /// <param name="command">The bundle creation command</param>
+        /// <returns>APIResponse containing the created bundle's ID</returns>
         [HttpPost]
         [Authorize(Roles = SD.Role_Admin)]
+        [ProducesResponseType(typeof(APIResponse<CreateBundleResponse>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(APIResponse<object>), (int)HttpStatusCode.BadRequest)]
         public async Task<ActionResult<CreateBundleResponse>> CreateBundle([FromForm] CreateBundleCommand command)
         {
             var response = await Mediator.Send(command);
             return Ok(response);
+        }
+
+        /// <summary>
+        /// Get all bundles
+        /// </summary>
+        /// <returns>APIResponse containing a list of all bundles</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(APIResponse<IList<CreateBundleResponse>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(APIResponse<object>), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetAllBundles()
+        {
+            var result = await Mediator.Send(new GetAllBundles.GetAllBundlesQuery());
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Get a specific bundle by Id
+        /// </summary>
+        /// <param name="id">The bundle Id</param>
+        /// <returns>APIResponse containing the bundle details</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(APIResponse<CreateBundleResponse>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(APIResponse<object>), (int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(APIResponse<object>), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetBundleById(Guid id)
+        {
+            var result = await Mediator.Send(new GetBundleById.GetBundleByIdQuery { Id = id });
+            return Ok(result);
         }
     }
 }
