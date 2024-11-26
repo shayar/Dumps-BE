e6d2aeaf82eb79db16bd1660374378a784a0e45f
Shayar Shrestha <shayarshrestha23@gmail.com>
2024-11-26 11:25:36 -0500

    API for checking the user is admin or not

---
+++ src/Presentation/Dumps.API/Controllers/RolesController.cs
@@ -0,0 +1,22 @@
+﻿using Dumps.Domain.Common.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dumps.API.Controllers
+{
+    public class RolesController : BaseController
+    {
+        /// <summary>
+        /// Check if the current user is an Admin.
+        /// </summary>
+        /// <returns>APIResponse indicating whether the user is an Admin.</returns>
+        [HttpGet("is-admin")]
+        [Authorize]
+        [ProducesResponseType(typeof(bool), 200)]
+        public IActionResult Index()
+        {
+            var isAdmin = User.IsInRole(SD.Role_Admin);
+            return Ok(new { IsAdmin = isAdmin });
+        }
+    }
+}
