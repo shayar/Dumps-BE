cb09871d5f04b6b725fef39547e9920d9754f518
Shayar Shrestha <shayarshrestha23@gmail.com>
2025-04-12 14:22:16 -0400

    Adding forget and reset password functionality

---
+++ src/Core/Dumps.Application/DTO/Request/Account/ResetPasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Dumps.Application.DTO.Request.Account
+{
+    public class ResetPasswordRequest
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
